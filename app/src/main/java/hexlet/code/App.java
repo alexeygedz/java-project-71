6dd0fac19c17e79fead859c9e149c766cf49a208
gedz <gedz@arnica.pro>
2026-09-25 19:08:23 +0500

    feat: add reads and parses JSON files

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,16 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 
@@ -31,12 +37,29 @@ public class App implements Callable<Integer> {
     private String format;
 
     @Override
-    public Integer call() {
+    public Integer call() throws Exception {
+        Map<String, Object> data1 = parseFile(filepath1);
+        Map<String, Object> data2 = parseFile(filepath2);
+
         return 0;
     }
 
+    private Map<String, Object> parseFile(String filepath) throws Exception {
+        Path path = Paths.get(filepath).toAbsolutePath().normalize();
+        String content = Files.readString(path);
+
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(content, new TypeReference<Map<String, Object>>() {});
+    }
+
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new App()).execute(args);
-        System.exit(exitCode);
+        try {
+            int exitCode = new CommandLine(new App()).execute(args);
+            System.exit(exitCode);
+        } catch (Exception e) {
+            System.err.println("Error " + e.getMessage());
+            System.exit(1);
+        }
+
     }
 }
