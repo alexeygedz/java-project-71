b6a17efe41a5d6feba65f6848cc5ea6364742193
gedz <gedz@arnica.pro>
2026-09-25 12:58:19 +0500

    feat: update help output CLI

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,8 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 import java.util.concurrent.Callable;
 
@@ -13,6 +15,20 @@ import java.util.concurrent.Callable;
     description = "Compares two configuration files and shows a difference."
 )
 public class App implements Callable<Integer> {
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @Option(
+        names = {"-f", "--format"},
+        paramLabel = "format",
+        defaultValue = "stylish",
+        description = "output format [default: ${DEFAULT-VALUE}]"
+    )
+    private String format;
 
     @Override
     public Integer call() {
